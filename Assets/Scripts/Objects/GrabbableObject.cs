c9766af5cf15cbb3da383af762d0319ed40e90f1
nanda_gamedev <nandix.myroom@gmx.at>
2026-06-15 15:35:08 +0200

    Physics Driven Grabbing

--- Assets/Scripts/Objects/GrabbableObject.cs
+++ Assets/Scripts/Objects/GrabbableObject.cs
@@ -1,4 +1,4 @@
-using PlasticGui.WorkspaceWindow.Locks;
+using Gorpozon.WarehouseSim.Player;
 using UnityEngine;
 
 namespace Gorpozon.WarehouseSim.Objects
@@ -6,16 +6,11 @@ namespace Gorpozon.WarehouseSim.Objects
     [RequireComponent(typeof(Rigidbody))]
     public class GrabbableObject : MonoBehaviour, IInteractible
     {
-        [SerializeField] private float pickupDistance = 2f;
-        [SerializeField] private float rotationSpeed = 50f;
-        [SerializeField] private float distSpeed = 10f;
-
-        public string InteractionPrompt => isHeld ? "Drop" : "Pick up";
+        public string InteractionPrompt => "Pick up";
 
         public bool CanInteract => true;
 
         private Rigidbody rb;
-
         private bool isHeld;
 
         private void Awake()
@@ -23,42 +18,22 @@ namespace Gorpozon.WarehouseSim.Objects
             rb = GetComponent<Rigidbody>();
         }
 
-        private void Update()
-        {
-            if (!isHeld) return;
-
-            float scrollInput = Input.mouseScrollDelta.y * distSpeed * Time.deltaTime;
-
-            Vector3 rotInput = new Vector3(-Input.GetAxis("Horizontal"),
-                                           Input.GetAxis("Vertical"),
-                                           -Input.GetAxis("Roll"));
-
-            rotInput *= rotationSpeed * Time.deltaTime;
-
-            Transform camTrans = Camera.main.transform;
-
-            transform.RotateAround(transform.position, camTrans.right, rotInput.y);
-            transform.RotateAround(transform.position, camTrans.up, rotInput.x);
-            transform.RotateAround(transform.position, camTrans.forward, rotInput.z);
-
-            transform.position += camTrans.forward * scrollInput;
-        }
-
         public void Interact()
         {
             isHeld = !isHeld;
-            rb.isKinematic = isHeld;
+            rb.useGravity = !isHeld;
+            //rb.isKinematic = isHeld;
+        }
 
-            if (isHeld)
-            {
-                Transform camTransform = Camera.main.transform;
-                transform.SetParent(camTransform);
-                transform.localPosition = Vector3.forward * pickupDistance;
-            }
-            else
-            {
-                transform.SetParent(null);
-            }
+        public void MoveTowardsTarget(Vector3 targetPos, float grabForce)
+        {
+            Vector3 force = (targetPos - transform.position) * grabForce * 10 * Time.fixedDeltaTime;
+            rb.linearVelocity = force;
+        }
+
+        public void AddTorque(Vector3 axis, float torque)
+        {
+            rb.AddTorque(axis * torque, ForceMode.VelocityChange);
         }
     }
 }
--- Assets/Scripts/Player/InteractionHandler.cs
+++ Assets/Scripts/Player/InteractionHandler.cs
@@ -1,7 +1,6 @@
 using Gorpozon.WarehouseSim.Objects;
 using Gorpozon.WarehouseSim.UI;
 using SBG.ServiceLocating;
-using System.Diagnostics.Tracing;
 using UnityEngine;
 
 namespace Gorpozon.WarehouseSim.Player
@@ -11,6 +10,15 @@ namespace Gorpozon.WarehouseSim.Player
 		[SerializeField] private Transform viewCamera;
 		[SerializeField] private float interactionRange = 5f;
 		[SerializeField] private LayerMask interactionMask;
+        [SerializeField] private float objectGrabForce = 65;
+        [SerializeField] private float objectRotationSpeed = 20f;
+        [SerializeField] private float distanceAdjustSpeed = 5f;
+        [SerializeField] private float defaultHoldDistance = 2f;
+        [SerializeField] private float minHoldDistance = 0.5f;
+        [SerializeField] private float maxHoldDistance = 4f;
+
+        private GrabbableObject heldObject;
+        private float currentHoldDistance;
 
 		private IInteractible currentTarget;
 		private HUD hud;
@@ -22,26 +30,64 @@ namespace Gorpozon.WarehouseSim.Player
 
         void Update()
 		{
-			if (Physics.Raycast(viewCamera.position, viewCamera.forward, out var hit, interactionRange, interactionMask))
-			{
+            if (heldObject != null) UpdateGrabbing();
+            else UpdateInteractions();
+		}
+
+        private void UpdateGrabbing()
+        {
+            float scrollInput = Input.mouseScrollDelta.y * distanceAdjustSpeed * Time.deltaTime;
+
+            Vector3 rotInput = new Vector3(-Input.GetAxis("Horizontal"),
+                                           Input.GetAxis("Vertical"),
+                                           -Input.GetAxis("Roll"));
+
+            rotInput *= objectRotationSpeed * Time.deltaTime;
+
+            heldObject.AddTorque(viewCamera.right, rotInput.y);
+            heldObject.AddTorque(viewCamera.up, rotInput.x);
+            heldObject.AddTorque(viewCamera.forward, rotInput.z);
+
+            currentHoldDistance = Mathf.Clamp(currentHoldDistance+scrollInput, minHoldDistance, maxHoldDistance);
+
+            Vector3 targetPos = viewCamera.transform.position + (viewCamera.forward * currentHoldDistance);
+            heldObject.MoveTowardsTarget(targetPos, objectGrabForce);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                heldObject.Interact();
+                heldObject = null;
+            }
+        }
+
+		private void UpdateInteractions()
+		{
+            if (Physics.Raycast(viewCamera.position, viewCamera.forward, out var hit, interactionRange, interactionMask))
+            {
                 currentTarget = hit.collider.GetComponent<IInteractible>();
 
-				if (currentTarget != null && currentTarget.CanInteract)
-				{
-					hud.SetInteractionPrompt(currentTarget.InteractionPrompt);
-				}
-			}
-			else if (currentTarget != null)
-			{
+                if (currentTarget != null && currentTarget.CanInteract)
+                {
+                    hud.SetInteractionPrompt(currentTarget.InteractionPrompt);
+                }
+            }
+            else if (currentTarget != null)
+            {
                 hud.SetInteractionPrompt(string.Empty);
                 currentTarget = null;
             }
 
             if (Input.GetMouseButtonDown(0) && currentTarget != null && currentTarget.CanInteract)
-			{
-				currentTarget.Interact();
-			}
-		}
+            {
+                if (currentTarget is GrabbableObject)
+                {
+                    heldObject = currentTarget as GrabbableObject;
+                    currentHoldDistance = defaultHoldDistance;
+                    hud.SetInteractionPrompt("Drop");
+                }
+                currentTarget.Interact();
+            }
+        }
 
         private void OnDrawGizmosSelected()
         {

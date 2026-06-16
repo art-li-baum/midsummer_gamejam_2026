7b04ee61407c4b48eba055eddede32e374379866
Arthur Li Baum <art.li.baum@gmail.com>
2026-06-16 16:55:24 +0200

    added screen with level progression hooked up

---
+++ Assets/Scripts/UI/StatScreen.cs
@@ -0,0 +1,33 @@
+using Gorpozon.WarehouseSim.Management;
+using SBG.ServiceLocating;
+using TMPro;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+using UnityEngine;
+
+namespace Gorpozon.WarehouseSim.UI
+{ 
+	public class StatScreem: MonoBehaviour
+	{
+		[SerializeField] private TMP_Text levelAmmount;
+        [SerializeField] private TMP_Text glorpobuxAmmount;
+		[SerializeField] private TMP_Text packageAmmount;
+        [SerializeField] private TMP_Text shiftAmmount;
+
+		private ProgressionManager progressionManager;
+
+
+        void Start()
+		{
+			ServiceLocator.TryGet(out progressionManager);
+
+			levelAmmount.text = "1";
+			glorpobuxAmmount.text = "0";
+			packageAmmount.text = "0";
+			shiftAmmount.text = "0";
+
+			progressionManager.levelUpEvent += level => { levelAmmount.text =  (level + 1).ToString(); };
+		
+		}
+
+	}
+}
